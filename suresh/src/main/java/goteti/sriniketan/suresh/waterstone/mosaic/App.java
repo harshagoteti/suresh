9e21b858c6f87992bf10028d94f883746d318442
Cloud User <centos@ip-172-31-89-173.ec2.internal>
2018-07-04 08:01:36 +0000

    commit 1

---
+++ suresh/src/main/java/goteti/sriniketan/suresh/waterstone/mosaic/App.java
@@ -0,0 +1,13 @@
+package goteti.sriniketan.suresh.waterstone.mosaic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ suresh/src/test/java/goteti/sriniketan/suresh/waterstone/mosaic/AppTest.java
@@ -0,0 +1,20 @@
+package goteti.sriniketan.suresh.waterstone.mosaic;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
